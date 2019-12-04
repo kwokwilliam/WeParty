6c79f04600b7611ba603dc6935402122feccef79
aliar97 <aliar97@uw.edu>
2019-12-04 12:46:49 -0800

    got file writing working

---
+++ WeParty/app/src/main/java/edu/uw/wkwok16/weparty/DataService/PointsSingleton.kt
@@ -0,0 +1,27 @@
+package edu.uw.wkwok16.weparty.DataService
+
+import android.location.Location
+
+class PointsSingleton {
+    companion object CurrentPoints {
+        private var currentParty = ""
+        private var location = Location("")
+
+
+        fun setPartyId(partyId: PartyId) {
+            currentParty = partyId
+        }
+
+        fun getPartyId (): PartyId {
+            return currentParty
+        }
+
+        fun getCurrentLocation(): Location {
+            return location
+        }
+
+        fun setCurrentLocation(loc: Location) {
+            location = loc
+        }
+    }
+}
--- WeParty/app/src/main/java/edu/uw/wkwok16/weparty/FollowParty.kt
+++ WeParty/app/src/main/java/edu/uw/wkwok16/weparty/FollowParty.kt
@@ -2,9 +2,12 @@ package edu.uw.wkwok16.weparty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_follow_party.*
+import java.io.File
+import java.io.FileWriter
 
 class FollowParty : AppCompatActivity() {
-
+    var files: File? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_follow_party)
@@ -12,6 +15,21 @@ class FollowParty : AppCompatActivity() {
         val actionbar = supportActionBar
         actionbar!!.title = "Follow Party"
         actionbar.setDisplayHomeAsUpEnabled(true)
+        party_follow.setOnClickListener {
+            if(!filesDir.exists()){
+                filesDir.mkdirs()
+            }
+            val files = File(filesDir, "coordinates.txt")
+            this.files = files
+            var currentCode = input_code.text.toString() + ", "
+
+            if (this.files != null) {
+                val writer = FileWriter(this.files as File)
+                writer.write(currentCode)
+                writer.close()
+            }
+            finish()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
